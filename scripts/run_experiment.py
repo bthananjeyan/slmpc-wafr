a8707b828a54a58c56b08963147fc77f8e331ffa
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-01-16 10:49:26 -0800

    modularity

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -4,27 +4,36 @@ from slmpc.misc.experiment import Experiment
 from slmpc.envs.pointbot import PointBot
 from slmpc.controllers import LMPC, RandomController
 
-def config():
+def pointbot_config(exp_cfg):
+	exp_cfg.env = env = PointBot()
+	exp_cfg.save_dir = "logs/pointbot"
+
+def config(env_name, controller_type):
 	exp_cfg = DotMap
 	exp_cfg.samples_per_iteration = 10
 	exp_cfg.num_iterations = 10
-	exp_cfg.controller_type = "lmpc_expect"
-	exp_cfg.env = env = PointBot()
-	exp_cfg.save_dir = "pointbot"
-	exp_cfg.log_all_data = True
+	exp_cfg.controller_type = controller_type
+	exp_cfg.log_all_data = False
+	exp_cfg.env_name = env_name
+
+	if exp_cfg.env_name == "pointbot":
+		pointbot_config(exp_cfg)
+	else:
+		raise Exception("Unsupported environment.")
 
 	if exp_cfg.controller_type == "random":
-		exp_cfg.controller = RandomController(env)
+		exp_cfg.controller = RandomController(exp_cfg.env)
 	elif exp_cfg.controller_type == "lmpc_expect":
-		exp_cfg.controller = LMPC(env)
+		exp_cfg.controller = LMPC(exp_cfg.env)
 	else:
 		raise Exception("Unsupported controller.")
-
 
 	return exp_cfg
 
 if __name__ == '__main__':
-	exp_cfg = config()
+	env_name = "pointbot"
+	controller_type = "lmpc_expect"
+	exp_cfg = config(env_name, controller_type)
 	experiment = Experiment(exp_cfg.controller, exp_cfg.env, exp_cfg)
 	experiment.run()
 	experiment.plot_results()
