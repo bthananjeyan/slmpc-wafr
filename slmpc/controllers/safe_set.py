adc39b37c63638ec28c28a0986bb4387483100a5
Ashwin Balakrishna <abalakrishna@gmail.com>
2020-01-21 01:55:18 -0800

    parallel attempt 2

--- slmpc/controllers/safe_set.py
+++ slmpc/controllers/safe_set.py
@@ -1,8 +1,8 @@
 # TODO: add goal conditioned filter or something
 class SafeSet:
-	def __init__(self):
-		self.state_data = [] # nsamples X horizon X state_dim
-		self.cost_data = [] # nsamples X horizon
+	def __init__(self, state_data=[], cost_data=[]):
+		self.state_data = state_data # nsamples X horizon X state_dim
+		self.cost_data = cost_data # nsamples X horizon
 
 	def get_samples(self, t=None):
 		if idx is None:
@@ -13,6 +13,13 @@ class SafeSet:
 	def add_sample(self, sample):
 		self.state_data.append(sample['states'])
 		self.cost_data.append(sample['costs'])
+
+	def load_data(self, state_data, cost_data):
+		self.state_data = state_data
+		self.cost_data = cost_data
+
+	def get_data(self):
+		return (self.state_data, self.cost_data)
 
 
 
