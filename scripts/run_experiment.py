888c7b93870b32197844203446a793bf3ca145ff
Ashwin Balakrishna <abalakrishna@gmail.com>
2020-01-23 00:02:56 -0800

    limit SS and value updates

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -16,14 +16,20 @@ def pointbot_config(exp_cfg):
 	exp_cfg.demo_path = "demos/pointbot/demos_1.p"
 	exp_cfg.ss_approx_mode = "knn" # Should change to 'convex_hull'
 	exp_cfg.value_approx_mode = "pe" # could be linear too, but I am pretty sure knn is better
-	exp_cfg.variable_start_state = False
+	exp_cfg.variable_start_state = True
 	exp_cfg.variable_start_state_cost = "towards" # options are [indicator, nearest_neighbor, towards]
 	exp_cfg.soln_mode = "cem"
 	exp_cfg.alpha_thresh = 3
 	exp_cfg.parallelize_cem = False
-	exp_cfg.parallelize_rollouts = False
+	exp_cfg.parallelize_rollouts = True
 	exp_cfg.model_logdir = 'model_logs'
 	exp_cfg.optimizer_params = {"num_iters": 5, "popsize": 200, "npart": 1, "num_elites": 40, "plan_hor": 15, "per": 1, "alpha": 0.1, "extra_hor": 5} # These kind of work for pointbot?
+	exp_cfg.n_samples_start_state_opt = 5
+	exp_cfg.start_state_opt_success_thresh = 0.6
+	exp_cfg.ss_value_train_success_thresh = 0.9
+	exp_cfg.desired_starts = [[-75, 0, 0, 0] for _ in range(exp_cfg.num_iterations)] # Placeholder for now
+	exp_cfg.update_SS_and_value_func_CEM = False
+	exp_cfg.max_update_SS_value = 50
 	return PointBot()
 
 def cartpole_config(exp_cfg):
@@ -38,12 +44,18 @@ def cartpole_config(exp_cfg):
 	exp_cfg.value_approx_mode = "pe" # could be linear too, but I am pretty sure knn is better
 	exp_cfg.model_logdir = 'model_logs'
 	exp_cfg.optimizer_params = {"num_iters": 5, "popsize": 200, "npart": 1, "num_elites": 40, "plan_hor": 20, "per": 1, "alpha": 0.1, "extra_hor": 5} # These kind of work for cartpole
+	exp_cfg.n_samples_start_state_opt = 5
+	exp_cfg.start_state_opt_success_thresh = 0.6
+	exp_cfg.ss_value_train_success_thresh = 0.6
+	exp_cfg.desired_starts = [[-50, 0, 0, 0] for _ in range(exp_cfg.num_iterations)] # Placeholder for now
+	exp_cfg.update_SS_and_value_func_CEM = False
+	exp_cfg.max_update_SS_value = 50
 	return CartPole()
 
 
 def pointbot_exp1_config(exp_cfg):
-	exp_cfg.samples_per_iteration = 1
-	exp_cfg.num_iterations = 5
+	exp_cfg.samples_per_iteration = 5
+	exp_cfg.num_iterations = 15
 	from slmpc.envs.pointbot_const import GOAL_STATE
 	exp_cfg.goal_schedule = NoSwitchSchedule(None, GOAL_STATE)
 
@@ -76,11 +88,6 @@ def config(env_name, controller_type, exp_id):
 	exp_cfg.env_name = env.env_name
 	exp_cfg.ac_lb = env.action_space.low
 	exp_cfg.ac_ub = env.action_space.high
-	exp_cfg.n_samples_start_state_opt = 5
-	exp_cfg.start_state_opt_success_thresh = 0.6
-	exp_cfg.ss_value_train_success_thresh = 0.6
-	exp_cfg.desired_starts = [[-50, 0, 0, 0] for _ in range(exp_cfg.num_iterations)] # Placeholder for now
-	exp_cfg.update_SS_and_value_func_CEM = False
 
 	return exp_cfg, env
 
