b3c51a196e7279fc91b85376072449298c199d6c
Brijen Thananjeyan <bthananjeyan@berkeley.edu>
2020-01-26 19:39:11 -0800

    asdf

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,4 +1,4 @@
-# python -m scripts.run_experiment -env pointbot -ctrl lmpc_expect -exp_id p1
+python -m scripts.run_experiment -env pointbot -ctrl lmpc_expect -exp_id p1
 # python -m scripts.run_experiment -env cartpole -ctrl lmpc_expect -exp_id c1
 # python -m scripts.run_experiment -env reacher -ctrl lmpc_expect -exp_id r1
-python -m scripts.run_experiment -env nlinkarm -ctrl lmpc_expect -exp_id n1
+# python -m scripts.run_experiment -env nlinkarm -ctrl lmpc_expect -exp_id n1
